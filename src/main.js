0e1ac1f6acd68876b628c188a44f09e9f22befad
wuj <1450626722@qq.com>
2021-12-02 09:48:55 +0800

    ✨ feat(): 配置路由，vuex

--- src/main.js
+++ src/main.js
@@ -1,17 +1,18 @@
 import Vue from 'vue';
 import App from './App.vue';
-// import VueRouter from 'vue-router';
 import Antd from 'ant-design-vue';
 import 'ant-design-vue/dist/antd.css';
-
 import router from './router';
+import { VueAxios } from '@/utils/request';
+import store from './store';
 
 Vue.config.productionTip = false;
 
 Vue.use(Antd);
-// Vue.use(VueRouter);
+Vue.use(VueAxios, router);
 
 new Vue({
   router,
+  store,
   render: h => h(App),
 }).$mount('#app');
---
+++ src/store/modules/app.js
@@ -0,0 +1,5 @@
+import Vue from 'vue';
+
+const app = {};
+
+export default app;
