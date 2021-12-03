f84c2d9cd48075123cefc84bf346d0c82d4a6431
wuj <1450626722@qq.com>
2021-12-03 15:28:02 +0800

    ✨ feat(): 路由

--- src/router/modules/dashboard.js
+++ src/router/modules/dashboard.js
@@ -1,10 +1,23 @@
 import Dashboard from '@views/dashboard/Dashboard';
+import Main from '@views/dashboard/Main';
+import Details from '@views/dashboard/Details';
 
 const dashboard = [
   {
     path: '/dashboard',
     name: 'dashboard',
+    redirect: '/dashboard/main',
     component: Dashboard,
+    children: [
+      {
+        path: 'main',
+        component: Main,
+      },
+      {
+        path: 'details',
+        component: Details,
+      },
+    ],
   },
 ];
 
--- src/views/dashboard/Dashboard.vue
+++ src/views/dashboard/Dashboard.vue
@@ -1,8 +1,24 @@
 <template>
   <div>
-    <div class="warp">dashboardhahhahah</div>
-    <a-button type="primary"> Primary </a-button>
-    <a-button>Default</a-button>
+    <a-menu mode="horizontal">
+      <a-menu-item key="dashboard"> <router-link to="/dashboard/main">dashboard</router-link></a-menu-item>
+      <a-menu-item key="app"><router-link to="/dashboard/details">details</router-link> </a-menu-item>
+      <a-sub-menu>
+        <span slot="title" class="submenu-title-wrapper"><a-icon type="setting" />Navigation Three - Submenu</span>
+        <a-menu-item-group title="Item 1">
+          <a-menu-item key="setting:1"> Option 1 </a-menu-item>
+          <a-menu-item key="setting:2"> Option 2 </a-menu-item>
+        </a-menu-item-group>
+        <a-menu-item-group title="Item 2">
+          <a-menu-item key="setting:3"> Option 3 </a-menu-item>
+          <a-menu-item key="setting:4"> Option 4 </a-menu-item>
+        </a-menu-item-group>
+      </a-sub-menu>
+      <a-menu-item key="alipay">
+        <a href="https://antdv.com" target="_blank" rel="noopener noreferrer">Navigation Four - Link</a>
+      </a-menu-item>
+    </a-menu>
+    <router-view></router-view>
   </div>
 </template>
 
